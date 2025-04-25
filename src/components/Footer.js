f7f8ab1cdf468010b841af9b4018d44739218794
SilentCosmo <116125023+silentCosmo@users.noreply.github.com>
2025-04-25 15:26:43 +0530

    update_footer_icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import '../styles/Footer.css'
-import React from 'react'/* 
-import GitHubIcon from '@mui/icons-material/GitHub'; */
+import React from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';/* 
 import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import InstagramIcon from '@mui/icons-material/Instagram'; */
 
 
 
@@ -12,11 +12,11 @@ function Footer() {
   return (
     <div className='footer'>
         <div className="socialMedia">
-        <a href="https://twitter.com/silentcosmo_"><TwitterIcon style= {{ fontSize: '28' }}/></a>
-        <a href="https://www.facebook.com/ngkudallur"><FacebookIcon style= {{ fontSize: '28' }}/></a>
-        <a href="https://www.instagram.com/silentcosmo_"><InstagramIcon style= {{ fontSize: '28' }}/></a>
+        <a href="https://twitter.com/silentcosmox"><TwitterIcon style= {{ fontSize: '28' }}/></a>
+        <a href="https://github.com/silentcosmo"><GitHubIcon style= {{ fontSize: '28' }}/></a>
+        {/* <a href="https://www.facebook.com/ngkudallur"><FacebookIcon style= {{ fontSize: '28' }}/></a>
+        <a href="https://www.instagram.com/silentcosmo_"><InstagramIcon style= {{ fontSize: '28' }}/></a> */}
         <a href="https://www.linkedin.com/in/silentcosmo"><LinkedInIcon style= {{ fontSize: '28' }}/></a>
-        {/* <GitHubIcon/> */}
         </div>
         <p>&copy;{new Date().getFullYear()} silentCosmo</p>
     </div>
