a40fa08705363f37daf8c1a94b4fcdb1d2500be2
SilentCosmo <116125023+silentCosmo@users.noreply.github.com>
2025-04-25 15:08:24 +0530

    update_projectList_7

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -6,12 +6,51 @@ import Movieapp from '../assets/movieapp.png'
 import Netflix from '../assets/netflix.png'
 import YouTube from '../assets/youtube.png'
 import Calc from '../assets/calculator.png'
+import Qleads from '../assets/qleads.png'
 import Seti from '../assets/seticss1.png'
+import Helbeku from '../assets/helbeku.png'
+import Cyber from '../assets/cyber.png'
+import Sevana from '../assets/sevana.png'
+import Space from '../assets/space.png'
+import Soque from '../assets/soque.png'
 import Shop from '../assets/shopsyi.png'
 import Todo from '../assets/todo.png'
 import Old from '../assets/old.png'
+import Iiq from '../assets/iiq.png'
 
 export const ProjectList = [
+    {
+        name: 'Quantum Leads',
+        image: Qleads,
+        skills: 'Next.js',
+        url: 'https://quantumleads.in/',
+        github: "https://github.com/integraliQ/leads",
+        description: `Website for an advertising company.`
+    },
+    {
+        name: 'IntegraliQ',
+        image: Iiq,
+        skills: 'Next.js, React, MongoDB, App API Router Endpoints',
+        url: 'https://integraliq.in',
+        github: "https://github.com/xsilentCosmo/iiq",
+        description: `Website for a business incubation company.`
+    },
+    {
+        name: 'Cyber Secqube',
+        image: Cyber,
+        skills: 'Next.js',
+        url: 'https://cybersecqube.com/',
+        github: "https://github.com/integraliQ/secqube",
+        description: `Website for an Education institution.`
+    },
+    {
+        name: 'iiQ Space',
+        image: Space,
+        skills: 'Next.js, React, Payment Gateway Integration, Firebase auth, FirestoreDB',
+        url: 'https://iiqspace.com',
+        github: "https://github.com/integraliQ/spaces",
+        description: `Website for co-working space provider.`
+    },
     {
         name: 'Youtube Clone',
         image: YouTube,
@@ -20,6 +59,31 @@ export const ProjectList = [
         github: "https://github.com/silentCosmo/youtube",
         description: `It's a clone of youtube webapp with signup,channel creation, upload, delete, comment, edit and more.`
     },
+    {
+        name: 'Helbeku',
+        image: Helbeku,
+        skills: 'Next.js, Realtime database, Analytics & SEO',
+        url: 'https://helbeku.vercel.app',
+        github: "https://github.com/silentCosmo/helbeku",
+        description: `Helbeku is a free and engaging platform for meeting new people through chats. Discover a fun way to connect with strangers`
+    },
+    {
+        name: 'Sevana-mvp',
+        image: Sevana,
+        skills: 'Next.js',
+        url: 'https://onlinesevana.com/',
+        github: "https://github.com/integraliQ/csc",
+        description: `CSC website front-end mvp`
+    },
+    {
+        name: 'Soque-MVP',
+        image: Soque,
+        skills: 'Next.js, Next Auth, Google Auth, MongoDB, NodeMailer',
+        url: 'https://soque.netlify.app',
+        github: "https://github.com/silentCosmo/soque",
+        description: `Social media management app (Paused project)`
+    },
+    
     {
         name: 'NovaFrame',
         image: NovaFrame,
